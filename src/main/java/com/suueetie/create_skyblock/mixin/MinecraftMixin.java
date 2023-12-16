78af74b00c66af13630deb64393fd614eba45303
suu <suuwuueetie@gmail.com>
2023-12-16 11:14:15 -0800

    updated dependencies

--- src/main/java/com/suueetie/create_skyblock/mixin/MinecraftMixin.java
+++ src/main/java/com/suueetie/create_skyblock/mixin/MinecraftMixin.java
@@ -1,6 +1,6 @@
-package com.example.modid.mixin;
+package com.suueetie.create_skyblock.mixin;
 
-import com.example.modid.ExampleMod;
+import com.suueetie.create_skyblock.CreateSkyblock;
 
 import net.minecraft.client.Minecraft;
 
@@ -15,6 +15,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MinecraftMixin {
 	@Inject(method = "<init>", at = @At("TAIL"))
 	private void example$init(GameConfig gameConfig, CallbackInfo ci) {
-		ExampleMod.LOGGER.info("Hello from {}", ExampleMod.NAME);
+		CreateSkyblock.LOGGER.info("Hello from {}", CreateSkyblock.NAME);
 	}
 }
